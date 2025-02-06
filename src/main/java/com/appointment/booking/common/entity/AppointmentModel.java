b97a254eeb8c7ca34d4aba9d8648d99c7610d518
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2025-02-06 11:03:53 -0500

    Updated entity

--- src/main/java/com/appointment/booking/common/entity/AppointmentModel.java
+++ src/main/java/com/appointment/booking/common/entity/AppointmentModel.java
@@ -1,7 +1,19 @@
 package com.appointment.booking.common.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 @Getter
 @Setter
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "appointments")
 public class AppointmentModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
