4198f9e3a62d420b576d08893042c22cc5b24325
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2025-02-17 14:38:57 -0500

    Updated entity model

--- src/main/java/com/appointment/booking/repository/AppointmentRepository.java
+++ src/main/java/com/appointment/booking/repository/AppointmentRepository.java
@@ -1,8 +1,12 @@
 package com.appointment.booking.repository;
 
+import com.appointment.booking.common.entity.AppointmentModel;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 // @Repository annotation is used to indicate that the class provides the mechanism for storage, retrieval, search, update and delete operation on objects.
 @Repository
-public interface AppointmentRepository {
+public interface AppointmentRepository extends JpaRepository<AppointmentModel, Long> {
+
+
 }
