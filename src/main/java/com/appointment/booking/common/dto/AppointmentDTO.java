8ed79264eefc03e25c933d4756d5ac5ccac65584
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2025-02-20 16:25:32 -0500

    Updated entity model

--- src/main/java/com/appointment/booking/common/dto/AppointmentDTO.java
+++ src/main/java/com/appointment/booking/common/dto/AppointmentDTO.java
@@ -1,5 +1,26 @@
 package com.appointment.booking.common.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Data
+@AllArgsConstructor
+@Builder
 // @Data is a convenient shortcut annotation that bundles the features of @ToString, @EqualsAndHashCode, @Getter / @Setter and @RequiredArgsConstructor together
 public class AppointmentDTO {
+
+    private Long id;
+    private LocalDate appointmentDate;
+    private LocalTime appointmentTime;
+    private String patientName;
+    private String doctorName;
+    private String appointmentReason;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
 }
