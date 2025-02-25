e03d14f3f44175f3fe887f3c258296aff3d5f828
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2025-02-25 14:29:03 -0500

    Commented code

--- src/main/java/com/appointment/booking/service/AppointmentService.java
+++ src/main/java/com/appointment/booking/service/AppointmentService.java
@@ -1,6 +1,20 @@
 package com.appointment.booking.service;
 
+import com.appointment.booking.common.dto.AppointmentDTO;
+import com.appointment.booking.common.entity.AppointmentModel;
+
+import java.util.List;
+import java.util.Optional;
+
 // @Service annotation is used to indicate that the class provides the mechanism for storage, retrieval, search, update and delete operation on objects.
 public interface AppointmentService {
+
+    Optional<AppointmentModel> getAppointmentById(Long id);
+
+    List<AppointmentModel> getAllAppointments();
+
+    AppointmentDTO updateAppointment(Long id, AppointmentDTO appointmentDTO);
+
+    void deleteAppointment(Long id);
 
 }
